28b53add9e5241622c52aced1883c9b8269a538d
Animesh Risal <animeshrisal99@gmail.com>
2022-04-23 17:17:19 +0545

    Fix network call for typescript

--- client/src/helpers/networkCall.ts
+++ client/src/helpers/networkCall.ts
@@ -1,38 +1,26 @@
-function authHeader() {
-    const user = localStorage.getItem('user')
-    if (user) {
-        return { 'Authorization': `JWT ${JSON.parse(user).access}` };
-    } else {
-        return {};
-    }
+function authHeader(): HeadersInit {
+  const user = localStorage.getItem("user");
+  if (user) {
+    return { Authorization: `JWT ${JSON.parse(user).access}` };
+  } else {
+    return {};
+  }
+}
+export function authenticatedRequestGenerator(
+  values: any = {},
+  method = "POST"
+) {
+  const requestOption = {
+    method,
+    headers: authHeader(),
+  };
+
+  return { ...requestOption, body: values };
 }
 
-const camelToSnakeCase = (str: string) => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
-
-export function authenticatedRequestGenerator(values: any = {}, method = 'POST') {
-
-    const formData = new FormData();
-
-    const requestOption = {
-        method,
-        headers: authHeader(),
-    }
-
-    if (Object.keys(values).length !== 0) {
-        for (var key in values) {
-            formData.append(camelToSnakeCase(key), values[key]);
-        }
-    }
-
-    return { ...requestOption, body: formData }
+export function authenticatedGetRequestOption(): RequestInit {
+  return {
+    method: "GET",
+    headers: authHeader(),
+  };
 }
-
-export function authenticatedGetRequestOption() {
-    return {
-        method: 'GET',
-        headers: authHeader()
-    }
-}
-
-
-
