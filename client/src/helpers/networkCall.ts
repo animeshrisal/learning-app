0962c768fe46220a6d4ed0b643fe3a4179855e5c
Animesh Risal <animeshrisal99@gmail.com>
2022-04-24 12:52:50 +0545

    Add formdata or json support in networkCall.ts

--- client/src/helpers/networkCall.ts
+++ client/src/helpers/networkCall.ts
@@ -1,9 +1,10 @@
-function authHeader(): HeadersInit {
+function authHeader(contentType: string): HeadersInit {
   const user = localStorage.getItem("user");
 
   if (user) {
-    console.log(JSON.parse(user).token);
-    return { Authorization: JSON.parse(user).token };
+    return {
+      Authorization: JSON.parse(user).token,
+    };
   } else {
     return {};
   }
@@ -12,17 +13,34 @@ export function authenticatedRequestGenerator(
   values: any = {},
   method = "POST"
 ) {
-  const requestOption = {
-    method,
-    headers: authHeader(),
-  };
+  let requestOption = {};
+  if ("image" in values) {
+    const formData: FormData = new FormData();
 
-  return { ...requestOption, body: values };
+    if (Object.keys(values).length !== 0) {
+      for (var key in values) {
+        formData.append(key, values[key]);
+      }
+    }
+    requestOption = {
+      method,
+      headers: authHeader("multipart/form-data"),
+    };
+
+    return { ...requestOption, body: formData };
+  } else {
+    requestOption = {
+      method,
+      headers: authHeader("application/json"),
+    };
+
+    return { ...requestOption, body: JSON.stringify(values) };
+  }
 }
 
 export function authenticatedGetRequestOption(): RequestInit {
   return {
     method: "GET",
-    headers: authHeader(),
+    headers: authHeader("application/json"),
   };
 }
