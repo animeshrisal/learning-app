2fc44d97a5ffc211cfcde54e1d3a2bb6464f297e
Animesh Risal <animeshrisal99@gmail.com>
2022-04-24 12:52:20 +0545

    Add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,13 @@ import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import TeacherClassList from "./pages/teacher/ClassroomList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
       <BrowserRouter>
         <Routes>
           <Route path="/auth/login/" element={<Login />} />
