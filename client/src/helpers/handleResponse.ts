7361e59f76c30ee13d2cb231535a18c2d6a5af5d
Animesh Risal <animeshrisal99@gmail.com>
2022-04-24 19:52:54 +0545

    Fix handleResponse lint

--- client/src/helpers/handleResponse.ts
+++ client/src/helpers/handleResponse.ts
@@ -1,49 +1,46 @@
 export function handleResponse(response: any) {
-    return response.text().then((text: string) => {
-        const data = keysToCamel(text && JSON.parse(text));
+  return response.text().then((text: string) => {
+    const data = keysToCamel(text && JSON.parse(text));
 
-        if (!response.ok) {
-            if (response.status === 401 && data.code === 'token_not_valid') {
-                localStorage.removeItem("user");
-            }
-            const errors = data;
-            return Promise.reject(errors);
-        }
-        return data;
-    });
+    if (!response.ok) {
+      if (response.status === 401 && data.code === "token_not_valid") {
+        localStorage.removeItem("user");
+      }
+      const errors = data;
+      return Promise.reject(errors);
+    }
+    return data;
+  });
 }
 
 const toCamel = (s: string) => {
-    return s.replace(/([-_][a-z])/ig, ($1) => {
-        return $1.toUpperCase()
-            .replace('-', '')
-            .replace('_', '');
-    });
+  return s.replace(/([-_][a-z])/gi, ($1) => {
+    return $1.toUpperCase().replace("-", "").replace("_", "");
+  });
 };
 
 const isArray = function (a: any) {
-    return Array.isArray(a);
+  return Array.isArray(a);
 };
 
 const isObject = function (o: any) {
-    return o === Object(o) && !isArray(o) && typeof o !== 'function';
+  return o === Object(o) && !isArray(o) && typeof o !== "function";
 };
 
 const keysToCamel = function (o: any) {
-    if (isObject(o)) {
-        const n: any = {};
+  if (isObject(o)) {
+    const n: any = {};
 
-        Object.keys(o)
-            .forEach((k) => {
-                n[toCamel(k)] = keysToCamel(o[k]);
-            });
+    Object.keys(o).forEach((k) => {
+      n[toCamel(k)] = keysToCamel(o[k]);
+    });
 
-        return n;
-    } else if (isArray(o)) {
-        return o.map((i: any) => {
-            return keysToCamel(i);
-        });
-    }
+    return n;
+  } else if (isArray(o)) {
+    return o.map((i: any) => {
+      return keysToCamel(i);
+    });
+  }
 
-    return o;
+  return o;
 };
