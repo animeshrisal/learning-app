c4ae65c85505701375f37fb7cbe4fb65663dfcee
Animesh Risal <animeshrisal99@gmail.com>
2022-04-24 07:16:14 +0545

    Upgrade to react 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,14 +5,18 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!)
+ 
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  
 );
 
 // If you want your app to work offline and load faster, you can change
