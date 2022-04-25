74df1c0ad47c2998a7083b8340512cd963c10fd9
Animesh Risal <animeshrisal99@gmail.com>
2022-04-25 19:36:30 +0545

    Lint add quiz dialogue

--- client/src/components/AddQuizDialogue.tsx
+++ client/src/components/AddQuizDialogue.tsx
@@ -1,47 +1,52 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
-import React, { useState } from 'react';
-
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
 
 const AddQuizDialogue = (props: any) => {
+  const [name, setName] = useState<string>("");
 
-    const [name, setName] = useState<string>('')
+  const handleClose = () => {
+    props.handleClose();
+  };
 
-    const handleClose = () => {
-        props.handleClose()
-    };
+  const addQuiz = () => {
+    props.addQuiz({ name }, props.state);
+    setName("");
+    handleClose();
+  };
 
-    const addQuiz = () => {
-        props.addQuiz({ name }, props.state)
-        setName('')
-        handleClose()
-    }
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
 
-
-    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
-
-    return (
-        <Dialog open={props.openModal} onClose={handleClose}>
-            <DialogTitle>{props.state} Quiz</DialogTitle>
-            <DialogContent>
-                <TextField
-                    margin="normal"
-                    value={name}
-                    onChange={handleName}
-                    required
-                    fullWidth
-                    id="subject"
-                    label="Subject"
-                    name="subject"
-                    autoComplete="subject"
-                    autoFocus
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={addQuiz}>Add Quiz</Button>
-            </DialogActions>
-        </Dialog>
-    )
-}
+  return (
+    <Dialog open={props.openModal} onClose={handleClose}>
+      <DialogTitle>{props.state} Quiz</DialogTitle>
+      <DialogContent>
+        <TextField
+          margin="normal"
+          value={name}
+          onChange={handleName}
+          required
+          fullWidth
+          id="subject"
+          label="Subject"
+          name="subject"
+          autoComplete="subject"
+          autoFocus
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={addQuiz}>Add Quiz</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default AddQuizDialogue;
