090e36488645c3ab0e3d420d1041f80fde86fa23
Animesh Risal <animeshrisal99@gmail.com>
2022-04-25 19:08:30 +0545

    Add quiz dialogue

---
+++ client/src/components/AddQuizDialogue.tsx
@@ -0,0 +1,47 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import React, { useState } from 'react';
+
+
+const AddQuizDialogue = (props: any) => {
+
+    const [name, setName] = useState('')
+
+    const handleClose = () => {
+        props.handleClose()
+    };
+
+    const addQuiz = () => {
+        props.addQuiz({ name }, props.state)
+        setName('')
+        handleClose()
+    }
+
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+
+    return (
+        <Dialog open={props.openModal} onClose={handleClose}>
+            <DialogTitle>{props.state} Quiz</DialogTitle>
+            <DialogContent>
+                <TextField
+                    margin="normal"
+                    value={name}
+                    onChange={handleName}
+                    required
+                    fullWidth
+                    id="subject"
+                    label="Subject"
+                    name="subject"
+                    autoComplete="subject"
+                    autoFocus
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={addQuiz}>Add Quiz</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default AddQuizDialogue;
