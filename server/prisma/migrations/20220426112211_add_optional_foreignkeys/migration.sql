d48f6ffbb1f646a0ff82a374197b588f8b6d846a
Animesh Risal <animeshrisal99@gmail.com>
2022-04-26 17:07:28 +0545

    Add previous and next for questions

---
+++ server/prisma/migrations/20220426112211_add_optional_foreignkeys/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[nextId]` on the table `Question` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[previousId]` on the table `Question` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Question" ADD COLUMN     "nextId" TEXT,
+ADD COLUMN     "previousId" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Question_nextId_key" ON "Question"("nextId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Question_previousId_key" ON "Question"("previousId");
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_nextId_fkey" FOREIGN KEY ("nextId") REFERENCES "Question"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_previousId_fkey" FOREIGN KEY ("previousId") REFERENCES "Question"("id") ON DELETE SET NULL ON UPDATE CASCADE;
