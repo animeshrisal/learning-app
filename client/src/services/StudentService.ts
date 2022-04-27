88714edfee98ecb894502de84cb051bb9a19779d
Animesh Risal <animeshrisal99@gmail.com>
2022-04-27 18:06:59 +0545

    Create student services

---
+++ client/src/services/StudentService.ts
@@ -0,0 +1,141 @@
+import {
+  authenticatedGetRequestOption,
+  authenticatedRequestGenerator,
+  handleResponse,
+  URL,
+} from "../helpers";
+import { Classroom } from "../models/states/ClassroomState";
+import { Lesson } from "../models/states/LessonState";
+import { Quiz } from "../models/states/QuizState";
+
+const getClassrooms = (): Promise<Classroom[]> => {
+  return fetch(`${URL}/student/classroom/`, authenticatedGetRequestOption())
+    .then(handleResponse)
+    .then((classrooms) => {
+      return classrooms;
+    });
+};
+
+const getClassroom = (id: string): Promise<Classroom> => {
+  return fetch(
+    `${URL}/student/classroom/${id}/`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((classroom: Classroom) => {
+      return classroom;
+    });
+};
+
+const getEnrollmentStatus = (id: string): Promise<string> => {
+  return fetch(
+    `${URL}/student/classroom/${id}/enroll`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((enrollment: string) => {
+      return enrollment;
+    });
+};
+
+const getLessons = (classroomId: string): Promise<Lesson[]> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/lesson`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((lessons:Lesson[]) => {
+      return lessons;
+    });
+};
+
+const getUserLessons = (classroomId: string): Promise<Lesson[]> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/user_lesson`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((lessons: Lesson[]) => {
+      return lessons;
+    });
+};
+
+const getLesson = (classroomId: string, lessonId: string): Promise<Lesson> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/lesson/${lessonId}`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((lesson: Lesson) => {
+      return lesson;
+    });
+};
+
+const createEnrollment = (classroomId: string): Promise<string> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/enroll`,
+    authenticatedRequestGenerator({}, "POST")
+  )
+    .then(handleResponse)
+    .then((enrollment: string) => {
+      return enrollment;
+    });
+};
+
+const completeLesson = (classroomId: string, lessonId: string): Promise<string> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/lesson/${lessonId}/complete`,
+    authenticatedRequestGenerator({}, "POST")
+  )
+    .then(handleResponse)
+    .then((enrollment: string) => {
+      return enrollment;
+    });
+};
+
+const completeQuiz = (classroomId: string, quizId: string, quizState: any): Promise<any> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/quiz/${quizId}/complete`,
+    authenticatedRequestGenerator(quizState, "POST")
+  )
+    .then(handleResponse)
+    .then((quizResult: any) => {
+      return quizResult;
+    });
+};
+
+const getQuizzes = (classroomId: string): Promise<Quiz[]> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/student_quiz/`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((quizzes: Quiz[]) => {
+      return quizzes;
+    });
+};
+
+const getQuestions = (classroomId: string, quizId: string): Promise<Quiz> => {
+  return fetch(
+    `${URL}/student/classroom/${classroomId}/quiz/${quizId}/question/`,
+    authenticatedGetRequestOption()
+  )
+    .then(handleResponse)
+    .then((quiz: Quiz) => {
+      return quiz;
+    });
+};
+
+export const studentService = {
+  createEnrollment,
+  completeLesson,
+  completeQuiz,
+  getClassrooms,
+  getClassroom,
+  getEnrollmentStatus,
+  getLessons,
+  getLesson,
+  getUserLessons,
+  getQuizzes,
+  getQuestions,
+};
