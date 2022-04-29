1a0bbbe0aad3f6cc7df947f2c2f5d6ced9da5f00
Animesh Risal <animeshrisal99@gmail.com>
2022-04-29 15:57:54 +0545

    Remove completed from user lesson

---
+++ server/prisma/migrations/20220429101230_remove_completed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `completed` on the `UserLesson` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserLesson" DROP COLUMN "completed";
