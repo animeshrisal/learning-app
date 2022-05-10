9792e0e99f517a3df44618b1ab1954f28941bdea
Animesh Risal <animesh.function@gmail.com>
2022-05-10 12:23:16 +0545

    Create navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,37 @@
-const Navbar = (): JSX.Element => {
-  return <></>;
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { RootState } from "../app/store";
+import "./Navbar.scss";
+
+const Navbar = (props: any): JSX.Element => {
+  const auth = useSelector((reduxState: RootState) => reduxState.auth);
+
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        {auth.token ? (
+          <>
+            <li>
+              <Link to="/teacher">
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile">
+                Profile
+              </Link>
+            </li>
+            <li><span>Logout</span></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/auth/login">Login</Link></li>
+            <li><Link to="/auth/register">Register</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
