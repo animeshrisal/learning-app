4129c5e5ecb19d66762d2b0ec3cfb3b24a39ab2c
Animesh Risal <animesh.function@gmail.com>
2022-05-11 17:51:38 +0545

    Add list in classroom

--- client/src/pages/student/Classroom.tsx
+++ client/src/pages/student/Classroom.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -6,7 +5,10 @@ import { RootState } from "../../app/store";
 import { useDispatch, useSelector } from "react-redux";
 
 import { retrieveStudentLessonList } from "../../slice/lessonSlice";
-import { enrollStudent, retrieveStudentClassroom } from "../../slice/classroomSlice";
+import {
+  enrollStudent,
+  retrieveStudentClassroom,
+} from "../../slice/classroomSlice";
 import { Lesson } from "../../models/states/LessonState";
 
 const StudentClassroom = (props: any): JSX.Element => {
@@ -21,7 +23,9 @@ const StudentClassroom = (props: any): JSX.Element => {
   const dispatch = useDispatch();
 
   const isLoading = useSelector((state: RootState) => state.lesson.isLoading);
-  const lessonList: Lesson[] = useSelector((state: RootState) => state.lesson.lessonList);
+  const lessonList: Lesson[] = useSelector(
+    (state: RootState) => state.lesson.lessonList
+  );
 
   useEffect(() => {
     if (classroomId) {
@@ -32,7 +36,7 @@ const StudentClassroom = (props: any): JSX.Element => {
 
   const createEnrollment = () => {
     if (classroomId) {
-      dispatch(enrollStudent(classroomId))
+      dispatch(enrollStudent(classroomId));
     }
   };
 
@@ -48,7 +52,14 @@ const StudentClassroom = (props: any): JSX.Element => {
 
   if (classroom) {
     return (
-      <div></div>
+      <ul>
+        {lessonList.map((lesson) => (
+          <div>
+            <div>{lesson.name}</div>
+            <div>{lesson.description}</div>
+          </div>
+        ))}
+      </ul>
     );
   } else {
     return <div />;
