2574980768e6db7199dc5f0bf4fc84b5d75da716
Animesh Risal <animesh.function@gmail.com>
2022-05-19 14:32:29 +0545

    Add chakra

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,15 +5,18 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
