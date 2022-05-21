f7992d7866ee5fef12f233f0810c457d07586dcf
Animesh Risal <animesh.function@gmail.com>
2022-05-21 20:18:17 +0545

    Add register page

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -5,23 +5,26 @@ import { loginUser } from "../../slice/authSlice";
 import { RootState } from "../../app/store";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Spinner } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Spinner,
+  Stack,
+} from "@chakra-ui/react";
+import { TriangleDownIcon } from "@chakra-ui/icons";
+import { useForm } from "react-hook-form";
 
 function Register() {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
-
-  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setUsername(event.target.value);
-  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setEmail(event.target.value);
-  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setPassword(event.target.value);
-  const handlePasswordConfirmation = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setPasswordConfirmation(event.target.value);
+  const { register, handleSubmit, formState } = useForm();
+  const { isSubmitting } = formState;
 
   const dispatch = useDispatch();
   const authState = useSelector((state: RootState) => state.auth);
@@ -46,63 +49,107 @@ function Register() {
   }, [authState.token, authState.role, navigate]);
 
   const handleLogin = (e: any) => {
-    e.preventDefault();
-    dispatch(loginUser({ username, password }));
+    dispatch(loginUser(e));
   };
 
   return (
-    <div>
-      <div className="container">
-        <form className="login-form">
-          <h2>Register</h2>
-          <input
-            autoComplete="off"
-            type="text"
-            name="username"
-            onChange={handleUsername}
-            className="login-form-input"
-            placeholder="Enter a username"
-          />
-            <input
-            autoComplete="off"
-            type="email"
-            name="email"
-            onChange={handleEmail}
-            className="login-form-input"
-            placeholder="Enter your email"
-          />
-          <input
-            autoComplete="off"
-            type="password"
-            name="password"
-            onChange={handlePassword}
-            className="login-form-input"
-            placeholder="Enter your passsword"
-          />
-          <input
-            autoComplete="off"
-            type="password"
-            name="password"
-            onChange={handlePasswordConfirmation}
-            className="login-form-input"
-            placeholder="Type your password again"
-          />
-          <button onClick={handleLogin} className={"login-form-submit"}>
-            {authState.isLoading ? <Spinner /> : <span>Register</span>}
-          </button>
-          <Link to="/auth/forgot-password">
-            <span>Forgot Password</span>
-          </Link>
-          <Link to="/auth/login">
-            <span>Already a member ? Login</span>
-          </Link>
-
-        </form>
-        <div className="side">
-          <div className="side-content"></div>
-        </div>
-      </div>
-    </div>
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="60vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="teal.500" />
+        <Heading color="teal.400">Welcome</Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<TriangleDownIcon color="gray.300" />}
+                  />
+                  <Input
+                    {...register("username")}
+                    placeholder="Username"
+                    id="username"
+                    type="text"
+                    required
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<TriangleDownIcon color="gray.300" />}
+                  />
+                  <Input
+                    {...register("email")}
+                    placeholder="Email"
+                    id="email"
+                    type="email"
+                    required
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<TriangleDownIcon color="gray.300" />}
+                  />
+                  <Input
+                    {...register("password")}
+                    placeholder="Password"
+                    id="password"
+                    type="password"
+                    required
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<TriangleDownIcon color="gray.300" />}
+                  />
+                  <Input
+                    {...register("confirmPassword")}
+                    placeholder="Confirm Password"
+                    id="confirmPassword"
+                    type="password"
+                    required
+                  />
+                </InputGroup>
+              </FormControl>
+              <Flex justifyContent="flex-end">
+                <Button
+                  type="submit"
+                  colorScheme="teal"
+                  isLoading={isSubmitting}
+                >
+                  Register
+                </Button>
+              </Flex>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
 
