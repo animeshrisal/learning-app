0d98f6383e2a85be8c463f3e1cf40a6e16d0e43c
Animesh Risal <animesh.function@gmail.com>
2022-05-22 06:55:37 +0545

    Create question modal form

--- client/src/components/AddQuestionDialogue.tsx
+++ client/src/components/AddQuestionDialogue.tsx
@@ -1,20 +1,43 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import Textarea from "@uiw/react-md-editor/lib/components/TextArea/Textarea";
 import React, { useState } from "react";
-
+import { useForm } from "react-hook-form";
 
 const AddQuestionDialogue = (props: any) => {
-  const [question, setQuestion] = useState("");
-  const [firstChoice, setFirstChoice] = useState("");
-  const [secondChoice, setSecondChoice] = useState("");
-  const [thirdChoice, setThirdChoice] = useState("");
-  const [fourthChoice, setFourthChoice] = useState("");
-  const [correctChoice, setCorrectChoice] = useState("");
+  const { register, handleSubmit, formState, watch } = useForm();
+  const { isSubmitting } = formState;
 
   const handleClose = () => {
     resetForm();
     props.handleClose();
   };
 
-  const addQuestion = () => {
+  const addQuestion = (data: any) => {
+    const {
+      question,
+      firstChoice,
+      secondChoice,
+      thirdChoice,
+      fourthChoice,
+      correctChoice,
+    } = data;
+
     props.addQuestionToQuiz(
       {
         question,
@@ -29,31 +52,87 @@ const AddQuestionDialogue = (props: any) => {
     handleClose();
   };
 
-  const resetForm = () => {
-    setQuestion("");
-    setFirstChoice("");
-    setSecondChoice("");
-    setThirdChoice("");
-    setFourthChoice("");
-    setCorrectChoice("");
-  };
-
-  const handleQuestion = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setQuestion(e.target.value);
-  const handleFirstChoice = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setFirstChoice(e.target.value);
-  const handleSecondChoice = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setSecondChoice(e.target.value);
-  const handleThirdChoice = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setThirdChoice(e.target.value);
-  const handleFourthChoice = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setFourthChoice(e.target.value);
-  const handleCorrectChoice = (e: any) => 
-  {
-    setCorrectChoice(e.target.value);}
+  const resetForm = () => {};
 
   return (
-    <div>ASd</div>
+    <Modal isOpen={props.open} onClose={handleClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add Question</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box>
+            <form>
+              <Stack
+                spacing={4}
+                p="1rem"
+                backgroundColor="whiteAlpha.900"
+                boxShadow="md"
+              >
+                <FormControl>
+                  <Input
+                    {...register("question")}
+                    placeholder="Question"
+                    id="question"
+                    type="text"
+                  />
+                </FormControl>
+                <FormControl>
+                  <Input
+                    {...register("firstChoice")}
+                    placeholder="First Choice"
+                    id="firstChoice"
+                    type="text"
+                  />
+                </FormControl>
+                <FormControl>
+                  <Input
+                    {...register("secondChoice")}
+                    placeholder="Second Choice"
+                    id="secondChoice"
+                    type="text"
+                  />
+                </FormControl>
+                <FormControl>
+                  <Input
+                    {...register("thirdChoice")}
+                    placeholder="Third Choice"
+                    id="thirdChoice"
+                    type="text"
+                  />
+                </FormControl>
+                <FormControl>
+                  <Input
+                    {...register("fourthChoice")}
+                    placeholder="Fourth Choice"
+                    id="fourthChoice"
+                    type="text"
+                  />
+                </FormControl>
+                <FormControl>
+                  <RadioGroup>
+                    <Text>Choose the correct Choice</Text>
+                    <Stack direction="row">
+                      <Radio value="1">First</Radio>
+                      <Radio value="2">Second</Radio>
+                      <Radio value="3">Third</Radio>
+                      <Radio value="4">Fourth</Radio>
+                    </Stack>
+                  </RadioGroup>
+                </FormControl>
+              </Stack>
+            </form>
+          </Box>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="ghost" onClick={handleSubmit(addQuestion)}>Create</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 };
 
